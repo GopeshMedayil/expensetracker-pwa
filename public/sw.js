f606284ae298240fd6b455894e0b8a43a111efb3
GopeshMedayil <medayilgopesh@gmail.com>
2017-08-19 11:39:41 +0530

    Added service worker for offline functionality

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,34 @@
-self.addEventListener("install", function() {
-  console.log("Insatlled");
+"use strict";
+
+var cacheName = "sw-first",
+  urlsToCache = [
+    "/",
+    "index.html",
+    "static/js/bundle.js",
+    "static/media/logo.5d5d9eef.svg"
+  ];
+
+self.addEventListener("install", function(event) {
+  console.log("Installed");
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log("opened cache", cache);
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
+
 self.addEventListener("activate", function() {
-  console.log("activated");
+  console.log("Activated");
 });
-self.addEventListener("fetch", function() {
-  console.log("fetched");
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
